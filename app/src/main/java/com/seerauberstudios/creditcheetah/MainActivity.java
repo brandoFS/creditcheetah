0dffe88f5bc4ec1bffc6ae50ad0344602a03c370
Brando <brando.madden@gmail.com>
2015-05-12 09:04:16 -0700

    finished exp date input, need to add luhn check, data validation for button

--- app/src/main/java/com/seerauberstudios/creditcheetah/MainActivity.java
+++ app/src/main/java/com/seerauberstudios/creditcheetah/MainActivity.java
@@ -4,19 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 import com.seerauberstudios.creditcheetah.util.RegexUtil;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class MainActivity extends Activity {
 
-    private EditText ccNum, ccMonth, ccYEar, ccCVV;
+    private EditText ccNum, ccMonth, ccYear, ccCVV;
     private Button submitBtn;
     private ImageView ccLogo, cvvLogo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,10 @@ public class MainActivity extends Activity {
 
         ccLogo = (ImageView)findViewById(R.id.mainactivity_card_icon);
         cvvLogo = (ImageView)findViewById(R.id.mainactivity_cardcvv_icon);
+        ccNum = (EditText)findViewById(R.id.mainactivity_card_number);
+        ccCVV = (EditText)findViewById(R.id.mainactivity_cardcvv);
+        ccMonth = (EditText)findViewById(R.id.mainactivity_card_expiration_month);
+        ccYear = (EditText)findViewById(R.id.mainactivity_card_expiration_year);
         submitBtn = (Button)findViewById(R.id.mainMenu_submitButton);
         setupUI();
     }
@@ -32,7 +42,7 @@ public class MainActivity extends Activity {
 
 
     private void validateSubmit(){
-        submitBtn.isEnabled();
+        submitBtn.setEnabled(true);
 
     }
 
@@ -44,7 +54,7 @@ public class MainActivity extends Activity {
 
 
     private void setupUI(){
-        ((EditText)findViewById(R.id.mainactivity_card_number)).addTextChangedListener(new TextWatcher() {
+        ((EditText)findViewById(R.id.mainactivity_card_number)).addTextChangedListener(new TextWatcher() { //set text watcher to change card brand logo
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -57,21 +67,81 @@ public class MainActivity extends Activity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (s.length() < 4) {
+                if (s.length() < 6) { //change back to blank if they delete
                     ccLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.genericcard, null));
-                } else if (s.length() == 16) {
-                    setCardBrand(s.toString());
+                    cvvLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.cvv, null));
+                } else if (s.length() == 7) { //get first 6 for IIN
+                    setCardBrand(s.toString()); //check/set card brand
                 }
 
 
             }
         });
+        ((EditText)findViewById(R.id.mainactivity_card_expiration_month)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-    }
+            }
 
-    private void setCardBrand(String iin){
-       // ccLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.visa, null));
-        System.out.println("HERE!!!!!!!!!! " + iin);
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 1) {
+                    int month = Integer.parseInt(s.toString());
+                    if (month > 0 && month < 13) {
+                        //do date stuff
+                    } else {
+                        ccMonth.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+                        showError("Out of Range");
+                    }
+
+                }
+            }
+        });
+        ((EditText)findViewById(R.id.mainactivity_card_expiration_year)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 1) {
+                    int year = Integer.parseInt(s.toString());
+                    int currentYear = Calendar.getInstance(Locale.getDefault()).get(Calendar.YEAR) % 100; //Get current year divide by 100 to get last 2 digits
+                    if (year >= currentYear && year <= currentYear + 50) {
+                        //do date stuff
+                    } else {
+                        ccYear.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+                        showError("Out of Range");
+                    }
+
+                }
+            }
+        });
+
+
+        ((Button)findViewById(R.id.mainMenu_submitButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+
+        }
+
+    private void setCardBrand(String iin){ //Check card's using pattern matching then change card logo
+        Boolean amex = false;
 
         if(RegexUtil.VISA_PATTERN.matcher(iin).matches()){
             ccLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.visa, null));
@@ -81,6 +151,7 @@ public class MainActivity extends Activity {
         }
         else if(RegexUtil.AMEX_PATTERN.matcher(iin).matches()){
             ccLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.amex, null));
+            amex = true;
         }
         else if(RegexUtil.DISC_PATTERN.matcher(iin).matches()){
             ccLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.discover, null));
@@ -91,6 +162,16 @@ public class MainActivity extends Activity {
         else {
             ccLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.genericcard, null));
         }
+
+        if(amex){
+            ccNum.setFilters(new InputFilter[]{new InputFilter.LengthFilter(15)});
+            cvvLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.amexcvv, null));
+        }
+        else{
+            ccNum.setFilters(new InputFilter[]{new InputFilter.LengthFilter(16)});
+            cvvLogo.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.cvv, null));
+        }
+
 
     }
 
---
+++ app/src/main/java/com/seerauberstudios/creditcheetah/util/ExpirationUtil.java
@@ -0,0 +1,16 @@
+package com.seerauberstudios.creditcheetah.util;
+
+/**
+ * Created by brando on 5/11/15.
+ */
+public class ExpirationUtil {
+
+
+    public void ValidateDate(int month, int year){
+
+
+    }
+
+
+
+}
